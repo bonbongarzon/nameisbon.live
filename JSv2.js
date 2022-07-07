6d51f8e3defe9de40db1de6f5e4c8e7d2d608ea7
bonbongarzon <97497863+bonbongarzon@users.noreply.github.com>
2022-07-08 02:46:24 +0800

    Add files via upload

--- JSv2.js
+++ JSv2.js
@@ -1,4 +1,52 @@
-// const top = document.querySelector(".top");
+let mouseCursor = document.querySelector(".cursor");
+window.addEventListener('mousemove',cursor);
+
+function cursor(e){
+    console.log(e);
+    document.querySelector(".cursor").style.top = e.pageY + "px";
+    document.querySelector(".cursor").style.left = e.pageX + "px";
+    // document.querySelector(".cursor").style.mixBlendMode = "difference";
+}
+
+
+// const body = document.querySelector("body");
+
+
+
+
+
+
+const maincolor = "#fafafa";
+const darkcolor = "black"
+
+
+window.addEventListener("scroll",function(event){
+    var scroll = this.scrollY;
+    console.log(scroll);
+
+    if(scroll>50){
+
+        document.querySelector("html").style.backgroundColor = darkcolor;
+        document.querySelector("body").style.backgroundColor = darkcolor;
+
+        const nodeList = document.querySelectorAll("#noticestatement");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.color = maincolor;
+}
+        // document.querySelector("#noticestatement").style.color = maincolor;
+
+    } else {
+        document.querySelector("body").style.backgroundColor = null;
+        const nodeList = document.querySelectorAll("#noticestatement");
+        for (let i = 0; i < nodeList.length; i++) {
+            nodeList[i].style.color = null;
+          }
+    }
+})
+
+
+
+
 // const bottom = document.querySelector(".bottom");
 
 
@@ -16,18 +64,15 @@
 
 
 
-let mouseCursor = document.querySelector(".cursor");
-let navLinks = document.querySelectorAll('.nav-links li');
-window.addEventListener('mousemove',cursor);
-
-function cursor(e){
-    mouseCursor.style.top = e.pageY + "px";
-    mouseCursor.style.left = e.pageX +  "px";
-}
 
 
-// const cursor = document.querySelector('.cursor');
 
-// document.addEventListener('mousemove', e => {
-//     cursor.setAttribute("style", "top:" + (e.pageY - 10) + "px; left:" + (e.pageX - 10) + "px;")
+// document.addEventListener('mousemove',(e) =>{
+
+//     mouseCursor.getElementsByClassName.style.top = e.pageY + "px";
+//     mouseCursor.getElementsByClassName.style.left = e.pageX + "px";
+    
+
+
 // })
+
